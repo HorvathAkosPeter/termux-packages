d5d5396db30c1ba49852a2875943bd156be47c2c
Peter Horvath <horvath.akos.peter@gmail.com>
2026-08-06 02:52:16 +0200

    license syntax fix, checksum added, cleanup, updated include dirs monit version is now 6.0.0, buggy osx workaround removal inserted\ getloadavg shim Apply suggestions from @TomJo2000
    Co-authored-by: TomIO <43716232+TomJo2000@users.noreply.github.com>

--- packages/monit/build.sh
+++ packages/monit/build.sh
@@ -1,19 +1,19 @@
 TERMUX_PKG_HOMEPAGE=https://mmonit.com/monit/
 TERMUX_PKG_DESCRIPTION="Utility for managing and monitoring processes, programs, files, directories and filesystems"
-TERMUX_PKG_LICENSE="AGPL-3.0"
-TERMUX_PKG_LICENSE_FILE="COPYING"
+TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="5.33.0"
-TERMUX_PKG_SRCURL=https://mmonit.com/monit/dist/monit-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=SKIP_CHECKSUM
+TERMUX_PKG_VERSION="6.0.0"
+TERMUX_PKG_SRCURL="https://mmonit.com/monit/dist/monit-${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256="ddacd2a8120aeb2351e4486ee04a17782b5004aee99f2041d829bc4dcf2a5b3b"
 TERMUX_PKG_DEPENDS="libandroid-glob, libandroid-support, zlib, openssl, libcrypt"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --without-pam
---with-ssl=enabled
+--with-ssl=yes
 --with-ssl-dir=$TERMUX_PREFIX
 --with-ssl-incl-dir=$TERMUX_PREFIX/include
 --with-ssl-lib-dir=$TERMUX_PREFIX/lib
+--oldincludedir=$TERMUX_PREFIX/include
 --enable-optimized
 LIBS=-landroid-glob
 "
@@ -26,17 +26,10 @@ termux_step_pre_configure() {
 }
 
 termux_step_configure() {
+	"$TERMUX_PKG_SRCDIR/bootstrap"
 	"$TERMUX_PKG_SRCDIR/configure" \
-		--host=$TERMUX_HOST_PLATFORM \
-		--prefix=$TERMUX_PREFIX \
-		--sysconfdir=$TERMUX_PREFIX/etc \
-		$TERMUX_PKG_EXTRA_CONFIGURE_ARGS 2> >(grep -v "tput: unknown terminfo capability" >&2)
-
-	# Ensure HAVE_SSL is strictly enabled across generated configurations
-	for cfg in $(find . -name "config.h" -o -name "Config.h" -o -name "libmonit.h"); do
-		if [ -f "$cfg" ]; then
-			sed -i 's/#undef HAVE_SSL/#define HAVE_SSL 1/g' "$cfg"
-			sed -i 's/#define HAVE_SSL 0/#define HAVE_SSL 1/g' "$cfg"
-		fi
-	done
+		--host="$TERMUX_HOST_PLATFORM" \
+		--prefix="$TERMUX_PREFIX" \
+		--sysconfdir="$TERMUX_PREFIX/etc" \
+		$TERMUX_PKG_EXTRA_CONFIGURE_ARGS
 }
